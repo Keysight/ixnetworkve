82942c2a706cfbadf208d08fd23b86b3ddd9c63e
armdupre <a-joseph.dupre@keysight.com>
2023-05-05 23:38:18 -0400

    Split up main.tf into smaller files that can be more easily reusable across projects. Separate common VPC resources into a module for later reuse across projects. Update variable naming conventions to be more in line with future module creation and code reuse across projects.

--- google-cloud-platform/Deployment/Terraform/IxNetwork/IxNetwork-on-GCP-2-VMone-Demo-Use-Case/vpc.tf
+++ google-cloud-platform/Deployment/Terraform/IxNetwork/IxNetwork-on-GCP-2-VMone-Demo-Use-Case/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-	source = "../../modules/vpc"
+	source = "../../modules/gcp-3-vpcs-1-public-subnet-2-private-subnets"
 	LoginIdTag = local.LoginIdTag
 	PublicFirewallRuleSourceIpRanges = local.PublicFirewallRuleSourceIpRanges
 }
