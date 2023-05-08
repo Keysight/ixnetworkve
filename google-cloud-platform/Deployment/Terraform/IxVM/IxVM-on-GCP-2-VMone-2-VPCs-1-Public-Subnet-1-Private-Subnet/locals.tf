46bd0b1649586e7c1abf901c04b33393ade2841a
armdupre <a-joseph.dupre@keysight.com>
2023-05-08 15:49:34 -0400

    Initial Checkin.

---
+++ google-cloud-platform/Deployment/Terraform/IxVM/IxVM-on-GCP-2-VMone-2-VPCs-1-Public-Subnet-1-Private-Subnet/locals.tf
@@ -0,0 +1,27 @@
+locals {
+	AgentCustomImageFamilyName = var.AgentCustomImageFamilyName
+	AgentCustomImageProjectId = var.AgentCustomImageProjectId
+	AgentMachineType = var.AgentMachineType
+	AgentNetworkTargetTags = [
+		"${local.AppName}-agent"
+	]
+	AgentSerialPortEnable = true
+	Agent1Eth0PrivateIpAddress = "10.0.10.11"
+	Agent1Eth0PublicIpAddressName = "${local.AppName}-agent-01-eth0-ip-addr"
+	Agent1Eth1PrivateIpAddress = "10.0.2.15"
+	Agent1Eth1PrivateIpAliases = "/28"
+	Agent1InstanceName = "${local.AppName}-agent-01"
+	Agent2Eth0PrivateIpAddress = "10.0.10.12"
+	Agent2Eth0PublicIpAddressName = "${local.AppName}-agent-02-eth0-ip-addr"
+	Agent2Eth1PrivateIpAddress = "10.0.2.116"
+	Agent2Eth1PrivateIpAliases = "/28"
+	Agent2InstanceName = "${local.AppName}-agent-02"
+	AppName = "ixnetwork"
+	LoginIdTag = var.LoginIdTag
+	OwnerTag = var.OwnerTag
+	ProjectId = var.ProjectId
+	ProjectTag = var.ProjectTag
+	PublicFirewallRuleSourceIpRanges = var.PublicFirewallRuleSourceIpRanges
+	RegionName = var.RegionName
+	ZoneName = var.ZoneName
+}
---
+++ google-cloud-platform/Deployment/Terraform/IxVM/IxVM-on-GCP-2-VMone-2-VPCs-1-Public-Subnet-1-Private-Subnet/main.tf
@@ -0,0 +1,97 @@
+resource "google_compute_address" "Agent1Eth0PublicIpAddress" {
+	name = "${local.LoginIdTag}-${local.ProjectTag}-${local.Agent1Eth0PublicIpAddressName}"
+	region = local.RegionName
+	network_tier = "PREMIUM"
+	address_type = "EXTERNAL"
+}
+
+resource "google_compute_instance" "Agent1Instance" {
+	name = "${local.LoginIdTag}-${local.ProjectTag}-${local.Agent1InstanceName}"
+	zone = local.ZoneName
+	machine_type = local.AgentMachineType
+	boot_disk {
+		device_name = "persistent-disk-0"
+		auto_delete = true
+		initialize_params {
+			image = data.google_compute_image.AgentImage.id
+		}
+	}
+	allow_stopping_for_update = true
+	enable_display = true
+	network_interface {
+		network = module.vpc.PublicVpcNetwork.self_link
+		network_ip = local.Agent1Eth0PrivateIpAddress
+		subnetwork = module.vpc.PublicSubnet.self_link
+		access_config {
+			nat_ip = google_compute_address.Agent1Eth0PublicIpAddress.address
+			network_tier = "PREMIUM"
+		}
+	}
+	network_interface {
+		network = module.vpc.PrivateVpcNetwork.self_link
+		network_ip = local.Agent1Eth1PrivateIpAddress
+		subnetwork = module.vpc.PrivateSubnet.self_link
+		alias_ip_range {
+			ip_cidr_range = local.Agent1Eth1PrivateIpAliases
+		}
+	}
+	metadata = {
+		Owner = local.OwnerTag
+		Project = local.ProjectTag
+		serial-port-enable = local.AgentSerialPortEnable
+	}
+	tags = local.AgentNetworkTargetTags
+	labels = {
+		owner = replace(replace(local.OwnerTag, ".", "-"), "@", "-")
+		project = lower(local.ProjectTag)
+	}
+}
+
+resource "google_compute_address" "Agent2Eth0PublicIpAddress" {
+	name = "${local.LoginIdTag}-${local.ProjectTag}-${local.Agent2Eth0PublicIpAddressName}"
+	region = local.RegionName
+	network_tier = "PREMIUM"
+	address_type = "EXTERNAL"
+}
+
+resource "google_compute_instance" "Agent2Instance" {
+	name = "${local.LoginIdTag}-${local.ProjectTag}-${local.Agent2InstanceName}"
+	zone = local.ZoneName
+	machine_type = local.AgentMachineType
+	boot_disk {
+		device_name = "persistent-disk-0"
+		auto_delete = true
+		initialize_params {
+			image = data.google_compute_image.AgentImage.id
+		}
+	}
+	allow_stopping_for_update = true
+	enable_display = true
+	network_interface {
+		network = module.vpc.PublicVpcNetwork.self_link
+		network_ip = local.Agent2Eth0PrivateIpAddress
+		subnetwork = module.vpc.PublicSubnet.self_link
+		access_config {
+			nat_ip = google_compute_address.Agent2Eth0PublicIpAddress.address
+			network_tier = "PREMIUM"
+		}
+	}
+	network_interface {
+		network = module.vpc.PrivateVpcNetwork.self_link
+		network_ip = local.Agent2Eth1PrivateIpAddress
+		subnetwork = module.vpc.PrivateSubnet.self_link
+		alias_ip_range {
+			ip_cidr_range = local.Agent2Eth1PrivateIpAliases
+		}
+	}
+	metadata = {
+		Owner = local.OwnerTag
+		Project = local.ProjectTag
+		serial-port-enable = local.AgentSerialPortEnable
+	}
+	tags = local.AgentNetworkTargetTags
+	labels = {
+		owner = replace(replace(local.OwnerTag, ".", "-"), "@", "-")
+		project = lower(local.ProjectTag)
+	}
+}
