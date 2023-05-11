95eb9b9262dbe350338141239e8783a1b65e2a6e
armdupre <a-joseph.dupre@keysight.com>
2023-05-11 10:47:42 -0400

    Separate common App resources into a module for later reuse across projects. Separate common Agent resources into a module for later reuse across projects.

--- google-cloud-platform/Deployment/Terraform/IxNetwork/IxNetwork-on-GCP-1-VMone-2-VPCs-1-Public-Subnet-1-Private-Subnet/data.tf
+++
@@ -1,9 +0,0 @@
-data "google_compute_image" "AppImage" {
-	family = local.AppCustomImageFamilyName
-	project = local.AppCustomImageProjectId
-}
-
-data "google_compute_image" "AgentImage" {
-	family = local.AgentCustomImageFamilyName
-	project = local.AgentCustomImageProjectId
-}
--- google-cloud-platform/Deployment/Terraform/IxVM/IxVM-on-GCP-2-VMone-3-VPCs-1-Public-Subnet-2-Private-Subnets/data.tf
+++
@@ -1,4 +0,0 @@
-data "google_compute_image" "AgentImage" {
-	family = local.AgentCustomImageFamilyName
-	project = local.AgentCustomImageProjectId
-}
