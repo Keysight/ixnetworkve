a3ae272ec4b3a43edc24cefa6cd3dc25ed4345ea
armdupre <a-joseph.dupre@keysight.com>
2023-06-27 18:01:02 -0400

    Updated.

--- google-cloud-platform/Deployment/Terraform/IxNetwork/IxNetwork-on-GCP-2-VMone-2-VPCs-1-Public-Subnet-1-Private-Subnet/locals.tf
+++ google-cloud-platform/Deployment/Terraform/IxNetwork/IxNetwork-on-GCP-2-VMone-2-VPCs-1-Public-Subnet-1-Private-Subnet/locals.tf
@@ -1,18 +1,19 @@
 locals {
 	AgentMachineType = var.AgentMachineType
 	Agent2Eth0PrivateIpAddress = "10.0.10.12"
-	Agent2Eth0PublicIpAddressName = "${local.AppName}-${local.Agent2Id}-eth0-ip-addr"
+	Agent2Eth0PublicIpAddressName = "${local.Preamble}-eth0-ip-addr"
 	Agent2Eth1PrivateIpAddress = "10.0.2.116"
 	Agent2Eth1PrivateIpAliases = "/28"
-	Agent2Id = "agent-02"
-	Agent2InstanceName = "${local.AppName}-${local.Agent2Id}"
+	Agent2InstanceId = "agent-02"
+	Agent2InstanceName = "${local.Preamble}-instance"
 	AppMachineType = var.AppMachineType
 	AppName = "ixnetwork"
-	LoginIdTag = var.LoginIdTag
-	OwnerTag = var.OwnerTag
+	Preamble = "${local.UserLoginTag}-${local.UserProjectTag}-${local.AppName}-${local.Agent2InstanceId}"
 	ProjectId = var.ProjectId
-	ProjectTag = var.ProjectTag
 	PublicFirewallRuleSourceIpRanges = var.PublicFirewallRuleSourceIpRanges
 	RegionName = var.RegionName
+	UserEmailTag = var.UserEmailTag
+	UserLoginTag = var.UserLoginTag
+	UserProjectTag = var.UserProjectTag
 	ZoneName = var.ZoneName
 }
--- google-cloud-platform/Deployment/Terraform/IxNetwork/IxNetwork-on-GCP-2-VMone-2-VPCs-1-Public-Subnet-1-Private-Subnet/main.tf
+++ google-cloud-platform/Deployment/Terraform/IxNetwork/IxNetwork-on-GCP-2-VMone-2-VPCs-1-Public-Subnet-1-Private-Subnet/main.tf
@@ -1,55 +1,55 @@
-module "app" {
-	source = "../../modules/gcp-ixnetwork-app"
-	AppEth0SubnetName = module.vpc.PublicSubnet.name
-	AppEth0VpcNetworkName = module.vpc.PublicVpcNetwork.name
-	AppMachineType = local.AppMachineType
-	LoginIdTag = local.LoginIdTag
-	OwnerTag = local.OwnerTag
-	ProjectTag = local.ProjectTag
+module "App" {
+	source = "armdupre/module-ixnetwork-app/google"
+	Eth0SubnetName = module.Vpc.PublicSubnet.name
+	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
+	MachineType = local.AppMachineType
 	RegionName = local.RegionName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
 	ZoneName = local.ZoneName
 	depends_on = [
-		module.vpc.PublicSubnet,
-		module.vpc.PublicVpcNetwork
+		module.Vpc.PublicSubnet,
+		module.Vpc.PublicVpcNetwork
 	]
 }
 
-module "agent1" {
-	source = "../../modules/gcp-ixnetwork-agent"
-	AgentEth0SubnetName = module.vpc.PublicSubnet.name
-	AgentEth0VpcNetworkName = module.vpc.PublicVpcNetwork.name
-	AgentEth1SubnetName = module.vpc.PrivateSubnet.name
-	AgentEth1VpcNetworkName = module.vpc.PrivateVpcNetwork.name
-	AgentMachineType = local.AgentMachineType
-	LoginIdTag = local.LoginIdTag
-	OwnerTag = local.OwnerTag
-	ProjectTag = local.ProjectTag
+module "Agent1" {
+	source = "armdupre/module-ixnetwork-agent/google"
+	Eth0SubnetName = module.Vpc.PublicSubnet.name
+	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
+	Eth1SubnetName = module.Vpc.PrivateSubnet.name
+	Eth1VpcNetworkName = module.Vpc.PrivateVpcNetwork.name
+	MachineType = local.AgentMachineType
 	RegionName = local.RegionName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
 	ZoneName = local.ZoneName
 	depends_on = [
-		module.vpc.PublicSubnet,
-		module.vpc.PublicVpcNetwork
+		module.Vpc.PublicSubnet,
+		module.Vpc.PublicVpcNetwork
 	]
 }
 
-module "agent2" {
-	source = "../../modules/gcp-ixnetwork-agent"
-	AgentEth0PrivateIpAddress = local.Agent2Eth0PrivateIpAddress
-	AgentEth0SubnetName = module.vpc.PublicSubnet.name
-	AgentEth0VpcNetworkName = module.vpc.PublicVpcNetwork.name
-	AgentEth1PrivateIpAddress = local.Agent2Eth1PrivateIpAddress
-	AgentEth1PrivateIpAliases = local.Agent2Eth1PrivateIpAliases
-	AgentEth1SubnetName = module.vpc.PrivateSubnet.name
-	AgentEth1VpcNetworkName = module.vpc.PrivateVpcNetwork.name
-	AgentId = local.Agent2Id
-	AgentMachineType = local.AgentMachineType
-	LoginIdTag = local.LoginIdTag
-	OwnerTag = local.OwnerTag
-	ProjectTag = local.ProjectTag
+module "Agent2" {
+	source = "armdupre/module-ixnetwork-agent/google"
+	Eth0PrivateIpAddress = local.Agent2Eth0PrivateIpAddress
+	Eth0SubnetName = module.Vpc.PublicSubnet.name
+	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
+	Eth1PrivateIpAddress = local.Agent2Eth1PrivateIpAddress
+	Eth1PrivateIpAliases = local.Agent2Eth1PrivateIpAliases
+	Eth1SubnetName = module.Vpc.PrivateSubnet.name
+	Eth1VpcNetworkName = module.Vpc.PrivateVpcNetwork.name
+	InstanceId = local.Agent2InstanceId
+	MachineType = local.AgentMachineType
 	RegionName = local.RegionName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
 	ZoneName = local.ZoneName
 	depends_on = [
-		module.vpc.PublicSubnet,
-		module.vpc.PublicVpcNetwork
+		module.Vpc.PublicSubnet,
+		module.Vpc.PublicVpcNetwork
 	]
 }
