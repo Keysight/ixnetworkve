0cec8f1467ad10a624df0ecba5f32adc33cbcc11
armdupre <a-joseph.dupre@keysight.com>
2023-06-28 08:22:12 -0400

    Updated.

--- google-cloud-platform/Deployment/Terraform/IxNetwork/IxNetwork-on-GCP-2-VMone-3-VPCs-1-Public-Subnet-2-Private-Subnets/main.tf
+++ google-cloud-platform/Deployment/Terraform/IxNetwork/IxNetwork-on-GCP-2-VMone-3-VPCs-1-Public-Subnet-2-Private-Subnets/main.tf
@@ -1,55 +1,55 @@
-module "app" {
-	source = "../../modules/gcp-ixnetwork-app"
-	AppEth0SubnetName = module.vpc.PublicSubnet.name
-	AppEth0VpcNetworkName = module.vpc.PublicVpcNetwork.name
-	AppMachineType = local.AppMachineType
-	LoginIdTag = local.LoginIdTag
-	OwnerTag = local.OwnerTag
-	ProjectTag = local.ProjectTag
+module "App" {
+	source = "armdupre/module-ixnetwork-app/google"
+	Eth0SubnetName = module.Vpc.PublicSubnet.name
+	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
+	MachineType = local.AppMachineType
 	RegionName = local.RegionName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
 	ZoneName = local.ZoneName
 	depends_on = [
-		module.vpc.PublicSubnet,
-		module.vpc.PublicVpcNetwork
+		module.Vpc.PublicSubnet,
+		module.Vpc.PublicVpcNetwork
 	]
 }
 
-module "agent1" {
-	source = "../../modules/gcp-ixnetwork-agent"
-	AgentEth0SubnetName = module.vpc.PublicSubnet.name
-	AgentEth0VpcNetworkName = module.vpc.PublicVpcNetwork.name
-	AgentEth1SubnetName = module.vpc.Private1Subnet.name
-	AgentEth1VpcNetworkName = module.vpc.Private1VpcNetwork.name
-	AgentMachineType = local.AgentMachineType
-	LoginIdTag = local.LoginIdTag
-	OwnerTag = local.OwnerTag
-	ProjectTag = local.ProjectTag
+module "Agent1" {
+	source = "armdupre/module-ixnetwork-agent/google"
+	Eth0SubnetName = module.Vpc.PublicSubnet.name
+	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
+	Eth1SubnetName = module.Vpc.Private1Subnet.name
+	Eth1VpcNetworkName = module.Vpc.Private1VpcNetwork.name
+	MachineType = local.AgentMachineType
 	RegionName = local.RegionName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
 	ZoneName = local.ZoneName
 	depends_on = [
-		module.vpc.PublicSubnet,
-		module.vpc.PublicVpcNetwork
+		module.Vpc.PublicSubnet,
+		module.Vpc.PublicVpcNetwork
 	]
 }
 
-module "agent2" {
-	source = "../../modules/gcp-ixnetwork-agent"
-	AgentEth0SubnetName = module.vpc.PublicSubnet.name
-	AgentEth0VpcNetworkName = module.vpc.PublicVpcNetwork.name
-	AgentEth1SubnetName = module.vpc.Private2Subnet.name
-	AgentEth1VpcNetworkName = module.vpc.Private2VpcNetwork.name
-	AgentMachineType = local.AgentMachineType
-	AgentEth0PrivateIpAddress = local.Agent2Eth0PrivateIpAddress
-	AgentEth1PrivateIpAddress = local.Agent2Eth1PrivateIpAddress
-	AgentEth1PrivateIpAliases = local.Agent2Eth1PrivateIpAliases
-	AgentId = local.Agent2Id
-	LoginIdTag = local.LoginIdTag
-	OwnerTag = local.OwnerTag
-	ProjectTag = local.ProjectTag
+module "Agent2" {
+	source = "armdupre/module-ixnetwork-agent/google"
+	Eth0PrivateIpAddress = local.Agent2Eth0PrivateIpAddress
+	Eth0SubnetName = module.Vpc.PublicSubnet.name
+	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
+	Eth1PrivateIpAddress = local.Agent2Eth1PrivateIpAddress
+	Eth1PrivateIpAliases = local.Agent2Eth1PrivateIpAliases
+	Eth1SubnetName = module.Vpc.Private2Subnet.name
+	Eth1VpcNetworkName = module.Vpc.Private2VpcNetwork.name
+	InstanceId = local.Agent2InstanceId
+	MachineType = local.AgentMachineType
 	RegionName = local.RegionName
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
 	ZoneName = local.ZoneName
 	depends_on = [
-		module.vpc.PublicSubnet,
-		module.vpc.PublicVpcNetwork
+		module.Vpc.PublicSubnet,
+		module.Vpc.PublicVpcNetwork
 	]
 }
--- google-cloud-platform/Deployment/Terraform/IxNetwork/IxNetwork-on-GCP-2-VMone-3-VPCs-1-Public-Subnet-2-Private-Subnets/output.tf
+++ google-cloud-platform/Deployment/Terraform/IxNetwork/IxNetwork-on-GCP-2-VMone-3-VPCs-1-Public-Subnet-2-Private-Subnets/output.tf
@@ -1,22 +1,22 @@
 output "AgentImage" {
 	value = {
-		"family" : module.agent1.AgentImage.family
-		"name" : module.agent1.AgentImage.name
-		"project" : module.agent1.AgentImage.project
+		"family" : module.Agent1.Image.family
+		"name" : module.Agent1.Image.name
+		"project" : module.Agent1.Image.project
 	}
 }
 
 output "AppEth0PublicIpAddress" {
 	value = {
-		"address" : module.app.AppEth0PublicIpAddress.address
-		"address_type" : module.app.AppEth0PublicIpAddress.address_type
+		"address" : module.App.Eth0PublicIpAddress.address
+		"address_type" : module.App.Eth0PublicIpAddress.address_type
 	}
 }
 
 output "AppImage" {
 	value = {
-		"family" : module.app.AppImage.family
-		"name" : module.app.AppImage.name
-		"project" : module.app.AppImage.project
+		"family" : module.App.Image.family
+		"name" : module.App.Image.name
+		"project" : module.App.Image.project
 	}
 }
