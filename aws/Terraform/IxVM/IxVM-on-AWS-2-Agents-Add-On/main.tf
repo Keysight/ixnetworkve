6128a3e57a41d783cc9b227cba7645c51684f067
armdupre <a-joseph.dupre@keysight.com>
2023-08-16 15:43:23 -0400

    Updated.

--- aws/Terraform/IxVM/IxVM-on-AWS-2-Agents-Add-On/main.tf
+++ aws/Terraform/IxVM/IxVM-on-AWS-2-Agents-Add-On/main.tf
@@ -1,6 +1,6 @@
 module "Agent1" {
 	source = "armdupre/module-ixnetwork-agent/aws"
-	version = "9.36.1"
+	version = "9.36.0"
 	Eth0SecurityGroupId = local.PublicSecurityGroupId
 	Eth0SubnetId = local.PublicSubnetId
 	Eth1SecurityGroupId = local.PrivateSecurityGroupId
@@ -18,7 +18,7 @@ module "Agent1" {
 
 module "Agent2" {
 	source = "armdupre/module-ixnetwork-agent/aws"
-	version = "9.36.1"
+	version = "9.36.0"
 	Eth0PrivateIpAddress = local.Agent2Eth0PrivateIpAddress
 	Eth0SecurityGroupId = local.PublicSecurityGroupId
 	Eth0SubnetId = local.PublicSubnetId
