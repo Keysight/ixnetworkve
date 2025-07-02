1ac57f55fcca545d2c874d9611aa47420fd35957
armdupre <a-joseph.dupre@keysight.com>
2025-07-02 17:03:30 -0400

    Initial Checkin.

---
+++ microsoft-azure/Deployment/Terraform/IxNetworkWeb/IxNetworkWeb-on-Azure-1-App-2-Agents-1-Vnet-1-Public-Subnet-2-Private-Subnets/locals.tf
@@ -0,0 +1,25 @@
+locals {
+	AgentVmSize = var.AgentVmSize
+	Agent1InstanceId = "agent1"
+	Agent2Eth0IpAddress = "10.0.10.12"
+	Agent2Eth1IpAddresses = [ "10.0.3.12", "10.0.3.13", "10.0.3.14", "10.0.3.15", "10.0.3.16", "10.0.3.17", "10.0.3.18", "10.0.3.19", "10.0.3.20", "10.0.3.21" ]
+	Agent2InstanceId = "agent2"
+	AppAdminUserName = var.AppAdminUserName
+	AppTag = "ixnetwork"
+	AppVmSize = var.AppVmSize
+	ClientId = var.ClientId
+	ClientSecret = var.ClientSecret
+	Preamble = "${local.UserLoginTag}-${local.UserProjectTag}-${local.AppTag}"
+	PublicSecurityRuleSourceIpPrefixes = var.PublicSecurityRuleSourceIpPrefixes == null ? [ "${data.http.ip.response_body}/32" ] : var.PublicSecurityRuleSourceIpPrefixes
+	ResourceGroupLocation = var.ResourceGroupLocation
+	ResourceGroupName = var.ResourceGroupName == null ? "${local.Preamble}-resource-group" : var.ResourceGroupName
+	ResourceProviderRegistrations = var.ResourceProviderRegistrations
+	SshKeyAlgorithm = "RSA"
+	SshKeyName = "${local.Preamble}-ssh-key"
+	SshKeyRsaBits = "4096"
+	SubscriptionId = var.SubscriptionId
+	TenantId = var.TenantId
+	UserEmailTag = var.UserEmailTag == null ? data.azurerm_client_config.current.client_id : var.UserEmailTag
+	UserLoginTag = var.UserLoginTag == null ? "terraform" : var.UserLoginTag
+	UserProjectTag = var.UserProjectTag == null ? random_id.RandomId.id : var.UserProjectTag
+}
