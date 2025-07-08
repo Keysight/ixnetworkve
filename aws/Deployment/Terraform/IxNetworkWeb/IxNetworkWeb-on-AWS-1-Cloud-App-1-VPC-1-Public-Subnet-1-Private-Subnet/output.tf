14371cd8dc602aa77d45e004d9abc62cbde7254c
armdupre <a-joseph.dupre@keysight.com>
2025-07-08 14:29:21 -0400

    Initial Checkin.

---
+++ aws/Deployment/Terraform/IxNetworkWeb/IxNetworkWeb-on-AWS-1-Cloud-App-1-VPC-1-Public-Subnet-1-Private-Subnet/output.tf
@@ -0,0 +1,20 @@
+output "AppAmi" {
+	value = {
+		"image_id" : module.App.Ami.image_id
+		"name" : module.App.Ami.name
+		"owner_id" : module.App.Ami.owner_id
+	}
+}
+
+output "AppEth0ElasticIp" {
+	value = {
+		"public_dns" : module.App.Eth0ElasticIp.public_dns
+		"public_ip" : module.App.Eth0ElasticIp.public_ip
+	}
+}
+
+output "AvailabilityZones" {
+	value = {
+		"available.names" : data.aws_availability_zones.available.names
+	}
+}
---
+++ aws/Deployment/Terraform/IxNetworkWeb/IxNetworkWeb-on-AWS-1-Cloud-App-1-VPC-1-Public-Subnet-1-Private-Subnet/terraform.optional.auto.tfvars
@@ -0,0 +1,8 @@
+# ApiMaxRetries = 1
+# AppInstanceType = "t3.xlarge"
+# InboundIPv4CidrBlocks = [ "1.1.1.1/32" ]
+# PublicSubnetAvailabilityZone = "us-east-1a"
+# Region = "us-east-1"
+# UserEmailTag = "terraform@example.com"
+# UserLoginTag = "terraform"
+# UserProjectTag = "cloud-ist"
