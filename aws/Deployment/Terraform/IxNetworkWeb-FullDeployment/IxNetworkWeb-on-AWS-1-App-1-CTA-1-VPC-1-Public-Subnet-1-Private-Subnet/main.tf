48d8dbabd413d25ab987466627de826a71ff26d1
armdupre <a-joseph.dupre@keysight.com>
2026-01-22 13:42:09 -0500

    Initial Checkin.

---
+++ aws/Deployment/Terraform/IxNetworkWeb-FullDeployment/IxNetworkWeb-on-AWS-1-App-1-CTA-1-VPC-1-Public-Subnet-1-Private-Subnet/main.tf
@@ -0,0 +1,41 @@
+module "App" {
+	source = "git::https://github.com/Keysight/terraform-aws-module-ixnetwork-web-app.git?ref=26.0.0"
+	Eth0SecurityGroupId = module.Vpc.PublicSecurityGroup.id
+	Eth0SubnetId = module.Vpc.PublicSubnet.id
+	InstanceType = local.AppInstanceType
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	depends_on = [
+		module.Vpc
+	]
+}
+
+module "Agent1" {
+	source = "git::https://github.com/Keysight/terraform-aws-module-ixnetwork-cloud-agent.git?ref=26.0.0.spoke"
+	Eth0SecurityGroupId = module.Vpc.PublicSecurityGroup.id
+	Eth0SubnetId = module.Vpc.PublicSubnet.id
+	Eth1SecurityGroupId = module.Vpc.PrivateSecurityGroup.id
+	Eth1SubnetId = module.Vpc.PrivateSubnet.id
+	Eth2PrivateIpAddresses = local.Agent1Eth2PrivateIpAddresses
+	Eth2SecurityGroupId = module.Vpc.PrivateSecurityGroup.id
+	Eth2SubnetId = module.Vpc.PrivateSubnet.id
+	InstanceType = local.AgentInstanceType
+	PlacementGroupId = aws_placement_group.PlacementGroup.id
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	depends_on = [
+		aws_placement_group.PlacementGroup,
+		module.Vpc
+	]
+}
+
+resource "aws_placement_group" "PlacementGroup" {
+	name = local.PlacementGroupName
+	strategy = local.PlacementGroupStrategy
+}
+
+resource "random_id" "RandomId" {
+	byte_length = 4
+}
