b75fa318501369a002a7e746966d582bd97b0859
armdupre <a-joseph.dupre@keysight.com>
2026-02-12 08:02:26 -0500

    Updated.

--- google-cloud-platform/Deployment/Terraform/IxNetwork-TestApplianceOnly/IxNetwork-on-GCP-2-CTAs-3-VPCs-1-Public-Subnet-2-Private-Subnets/main.tf
+++ google-cloud-platform/Deployment/Terraform/IxNetwork-TestApplianceOnly/IxNetwork-on-GCP-2-CTAs-3-VPCs-1-Public-Subnet-2-Private-Subnets/main.tf
@@ -1,5 +1,5 @@
 module "Agent1" {
-	source = "git::https://github.com/armdupre/terraform-google-module-ixnetwork-cloud-agent.git?ref=11.0.1"
+	source = "git::https://github.com/Keysight/terraform-google-module-ixnetwork-cloud-agent.git?ref=26.0.0"
 	Eth0SubnetName = module.Vpc.PublicSubnet.name
 	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
 	Eth1SubnetName = module.Vpc.Private1Subnet.name
@@ -19,7 +19,7 @@ module "Agent1" {
 }
 
 module "Agent2" {
-	source = "git::https://github.com/armdupre/terraform-google-module-ixnetwork-cloud-agent.git?ref=11.0.1"
+	source = "git::https://github.com/Keysight/terraform-google-module-ixnetwork-cloud-agent.git?ref=26.0.0"
 	Eth0PrivateIpAddress = local.Agent2Eth0PrivateIpAddress
 	Eth0SubnetName = module.Vpc.PublicSubnet.name
 	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
