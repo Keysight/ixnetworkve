620eb3d68a1400ff55ba83a52406c1ba87d0a5db
armdupre <a-joseph.dupre@keysight.com>
2026-02-12 07:52:59 -0500

    Initial Checkin.

---
+++ google-cloud-platform/Deployment/Terraform/IxNetwork-FullDeployment/IxNetwork-on-GCP-1-App-1-VPC-1-Public-Subnet/main.tf
@@ -0,0 +1,19 @@
+module "App" {
+	source = "git::https://github.com/Keysight/terraform-google-module-ixnetwork-app.git?ref=26.0.0"
+	Eth0SubnetName = module.Vpc.PublicSubnet.name
+	Eth0VpcNetworkName = module.Vpc.PublicVpcNetwork.name
+	MachineType = local.AppMachineType
+	RegionName = data.google_client_config.current.region
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	ZoneName = data.google_client_config.current.zone
+	depends_on = [
+		module.Vpc.PublicSubnet,
+		module.Vpc.PublicVpcNetwork
+	]
+}
+
+resource "random_id" "RandomId" {
+	byte_length = 4
+}
