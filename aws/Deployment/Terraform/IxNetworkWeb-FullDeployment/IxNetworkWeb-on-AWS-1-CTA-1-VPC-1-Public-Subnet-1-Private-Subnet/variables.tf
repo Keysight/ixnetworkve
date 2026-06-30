4cd86d9a8f68430201c033430f13be4f28c13f42
armdupre <a-joseph.dupre@keysight.com>
2026-06-30 17:19:29 -0400

    Update variables.tf, variables.tf, variables.tf, and 4 more files

--- aws/Deployment/Terraform/IxNetworkWeb-FullDeployment/IxNetworkWeb-on-AWS-1-CTA-1-VPC-1-Public-Subnet-1-Private-Subnet/variables.tf
+++ aws/Deployment/Terraform/IxNetworkWeb-FullDeployment/IxNetworkWeb-on-AWS-1-CTA-1-VPC-1-Public-Subnet-1-Private-Subnet/variables.tf
@@ -1,38 +1,25 @@
+variable "AgentInstanceType" {
+	default = "c5n.2xlarge"
+	description = "Instance type of Agent VM"
+	type = string
+	validation {
+		condition = contains([ "c5n.2xlarge", "c5n.4xlarge", "c5n.9xlarge", "c5n.18xlarge",
+							   "c6in.2xlarge", "c6in.4xlarge", "c6in.8xlarge", "c6in.12xlarge", "c6in.16xlarge", "c6in.24xlarge", "c6in.32xlarge" ], var.AgentInstanceType)
+		error_message = <<EOF
+AgentInstanceType must be one of the following types:
+	c5n.2xlarge, c5n.4xlarge, c5n.9xlarge, c5n.18xlarge,
+	c6in.2xlarge, c6in.4xlarge, c6in.8xlarge, c6in.12xlarge, c6in.16xlarge, c6in.24xlarge, c6in.32xlarge
+		EOF
+	}
+}
+
 variable "ApiMaxRetries" {
 	default = 1
 	type = number
 }
 
-variable "AppInstanceType" {
-	default = "c5n.2xlarge"
-	description = "Instance type of App VM"
-	type = string
-	validation {
-		condition = contains([	
-								"c5.2xlarge",
-								"c5n.2xlarge"
-							], var.AppInstanceType)
-		error_message = <<EOF
-AppInstanceType must be one of the following types:
-	t3a.xlarge, t3a.2xlarge,
-	m6i.xlarge, m6i.2xlarge,
-	m6a.xlarge, m6a.2xlarge,
-	m6in.xlarge, m6in.2xlarge,
-	m5.xlarge, m5.2xlarge,
-	m5a.xlarge, m5a.2xlarge,
-	m5n.xlarge, m5n.2xlarge,
-	c6i.xlarge, c6i.2xlarge,
-	c6a.xlarge, c6a.2xlarge,
-	c6in.xlarge, c6in.2xlarge,
-	c5.xlarge, c5.2xlarge,
-	c5a.xlarge, c5a.2xlarge,
-	c5n.xlarge, c5n.2xlarge
-		EOF
-	}
-}
-
 variable "InboundIPv4CidrBlocks" {
-	default = null
+	default = [ "0.0.0.0/0" ]
 	description = "List of IP Addresses /32 or IP CIDR ranges connecting inbound to App"
 	type = list(string)
 }
@@ -44,6 +31,7 @@ variable "PrivateSubnetAvailabilityZone" {
 
 variable "PublicSubnetAvailabilityZone" {
 	default = "us-east-1a"
+	description = "AZ id associated with the public subnet"
 	type = string
 }
 
