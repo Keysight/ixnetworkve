16c41a5e943edf7db3ff75977e23dc53fcb880be
armdupre <a-joseph.dupre@keysight.com>
2026-07-08 14:08:56 -0400

    Update vnet.tf, vnet.tf, vnet.tf, and 12 more files

--- microsoft-azure/Deployment/Terraform/IxNetworkWeb-FullDeployment/IxNetworkWeb-on-Azure-1-App-1-VTA-1-Vnet-1-Public-Subnet-1-Private-Subnet/main.tf
+++ microsoft-azure/Deployment/Terraform/IxNetworkWeb-FullDeployment/IxNetworkWeb-on-Azure-1-App-1-VTA-1-Vnet-1-Public-Subnet-1-Private-Subnet/main.tf
@@ -1,5 +1,5 @@
 module "App" {
-	source = "git::https://github.com/Keysight/terraform-azurerm-module-ixnetwork-web-app.git?ref=26.0.0"
+	source = "git::https://github.com/Keysight/terraform-azurerm-module-ixnetwork-web-app.git?ref=26.3.0"
 	AdminUserName = local.AppAdminUserName
 	Eth0SubnetId = module.Vnet.PublicSubnet.id
 	ResourceGroupLocation = azurerm_resource_group.ResourceGroup.location
@@ -16,7 +16,7 @@ module "App" {
 }
 
 module "Agent1" {
-	source = "git::https://github.com/Keysight/terraform-azurerm-module-ixnetwork-agent.git?ref=26.0.0"
+	source = "git::https://github.com/Keysight/terraform-azurerm-module-ixnetwork-agent.git?ref=26.3.0"
 	Eth0SubnetId = module.Vnet.PublicSubnet.id
 	Eth1SubnetId = module.Vnet.PrivateSubnet.id
 	InstanceId = local.Agent1InstanceId
