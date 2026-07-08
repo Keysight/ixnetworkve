ebc3311ca5b2256e03603e06be1276c4a4b5462b
armdupre <a-joseph.dupre@keysight.com>
2026-07-08 14:07:55 -0400

    Update variables.tf, variables.tf, variables.tf, and 17 more files

--- microsoft-azure/Deployment/Terraform/IxNetwork-TestApplianceOnly/IxNetwork-on-AzureGovCloud-1-VTA-1-Vnet-1-Public-Subnet-1-Private-Subnet/variables.tf
+++ microsoft-azure/Deployment/Terraform/IxNetwork-TestApplianceOnly/IxNetwork-on-AzureGovCloud-1-VTA-1-Vnet-1-Public-Subnet-1-Private-Subnet/variables.tf
@@ -18,7 +18,7 @@ variable "Environment" {
 }
 
 variable "PublicSecurityRuleSourceIpPrefixes" {
-	default = null
+	default = [ "0.0.0.0/0" ]
 	description = "List of IP Addresses /32 or IP CIDR ranges connecting inbound to App"
 	type = list(string)
 }
@@ -39,12 +39,20 @@ variable "UserEmailTag" {
 	default = null
 	description = "Email address tag of user creating the deployment"
 	type = string
+	validation {
+		condition = var.UserEmailTag == null ? true : length(var.UserEmailTag) >= 14
+		error_message = "UserEmailTag minimum length must be >= 14."
+	}
 }
 
 variable "UserLoginTag" {
 	default = null
 	description = "Login ID tag of user creating the deployment"
 	type = string
+	validation {
+		condition = var.UserLoginTag == null ? true : length(var.UserLoginTag) >= 4
+		error_message = "UserLoginTag minimum length must be >= 4."
+	}
 }
 
 variable "UserProjectTag" {
